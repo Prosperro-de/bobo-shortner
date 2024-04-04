04c9d79d7c4ecea392cffa0b2e3c6da70bf4cf64
ibolshak <i.bolshak@comparus.de>
2024-04-04 08:39:05 +0300

    shortner

--- src/main/java/org/example/boboshortner/service/ShortnerService.java
+++ src/main/java/org/example/boboshortner/service/ShortnerService.java
@@ -1,15 +1,36 @@
 package org.example.boboshortner.service;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class ShortnerService {
-    private Map<String, String> context;
+
+    @Value("${base-url}")
+    private String baseUrl;
+
+    private Map<String, String> context = new HashMap<>();
 
     public String createShortUrl(String fullUrl) {
-        return "";
+
+        String key = RandomStringUtils.randomAlphabetic(10).toLowerCase();
+        while (context.containsKey(key)) {
+            key = RandomStringUtils.randomAlphabetic(10).toLowerCase();
+        }
+        context.put(key, fullUrl);
+        String url = baseUrl + "/" + key;
+        return url;
     }
 
     public String getFullUrl(String shortUrl) {
-        return "";
+        URI uri = URI.create(shortUrl);
+        String key = uri.getPath().replace("/", "");
+        String url = context.get(key);
+        return url;
     }
 }
